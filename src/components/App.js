ff974f81f04ae12991ee90eed884e665aa62e108
Shevchenko Elena <elena.kadru@gmail.com>
2020-09-14 00:40:42 +0300

    added chunkNames

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React, { Suspense, lazy } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Nav from './nav/Nav';
 
-const HomePage = lazy(() => import('./homePage/HomePage'));
-const MoviesPage = lazy(() => import('./moviesPage/MoviesPage'));
+const HomePage = lazy(() =>
+  import('./homePage/HomePage' /* webpackChunkName: "home-page" */),
+);
+const MoviesPage = lazy(() =>
+  import('./moviesPage/MoviesPage' /* webpackChunkName: "movies-page" */),
+);
 const MovieDetailsPage = lazy(() =>
-  import('./movieDetailsPage/MovieDetailsPage'),
+  import(
+    './movieDetailsPage/MovieDetailsPage' /* webpackChunkName: "movie-details-page" */
+  ),
 );
 const PageNotFound = lazy(() => {
   return <h2>Page not found ......</h2>;
